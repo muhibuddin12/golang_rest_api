355ce1134b5bf9118932a907d65cfb3ccc148b08
muhibuddin12 <muhibuddin14@gmail.com>
2023-05-28 15:16:46 +0700

    Create api documentation

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 	"github.com/muhibuddin12/golang_rest_api/helper"
 )
 
+func Pow(number *int) {
+	*number *= *number
+}
+
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(helper.SayHello("Muhibuddin"))
+
+	number := 12
+
+	Pow(&number)
+	fmt.Println(number)
 }
